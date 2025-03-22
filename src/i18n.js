eeb90ac5bd7f39c2c8b0a07b909d298a59bf0fc9
Eri Kurti <82323663+ErjonKurti@users.noreply.github.com>
2025-03-22 22:19:53 +0100

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from './locales/en/translation.json';
+import sqTranslations from './locales/sq/translation.json';
 
 i18n
     .use(initReactI18next)
